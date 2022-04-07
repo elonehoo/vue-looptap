682e100786431ef7394bb279c9b7220f2af9a4d6
xiaoxunyao <huchengyea@163.com>
2022-04-07 15:03:41 +0800

    fix: update

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -13,7 +13,7 @@ import { isDark, toggleDark } from '~/composables'
       class="icon-btn"
       i-carbon-logo-github
       rel="noreferrer"
-      href="https://github.com/xiaoxunyao/viden"
+      href="https://github.com/elonehoo/vue-looptap"
       target="_blank"
       title="GitHub"
     />
